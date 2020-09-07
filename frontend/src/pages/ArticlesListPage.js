2e88dc9af51fc861da74bfe72521f1de0ba9c8d9
Adam Vorkel <adamvorkel@gmail.com>
2020-09-07 17:56:15 +0200

    created reusable component for articles list

--- frontend/src/pages/ArticlesListPage.js
+++ frontend/src/pages/ArticlesListPage.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+
+import ArticlesList from '../components/ArticlesList';
 import dummyArticles from '../dummy-articles';
 
 const ArticlesListPage = () => (
     <>
         <h1>Articles</h1>
-        {dummyArticles.map((article, key) => (
-            <Link key={key} to={`/article/${article.name}`}>
-                <h3>{article.title}</h3>
-                <p>{article.content[0].substring(0, 150)}...</p>
-            </Link>
-        ))}
+        <ArticlesList articles={dummyArticles} />
     </>
 );
 
