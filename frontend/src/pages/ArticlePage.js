a69224524dce678519d5128524978c67a48d3406
Adam Vorkel <adamvorkel@gmail.com>
2020-09-07 17:42:44 +0200

    added dummy article content, article page loads articles dynamically according to url param

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import dummyArticles from '../dummy-articles';
 
-const ArticlePage = () => (
-    <>
-        <h1>This is an article</h1>
-    </>
-);
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    const article = dummyArticles.find(dummyArticle => dummyArticle.name === name);
+
+    if(!article) return <h1>Article does not exist</h1>
+
+    return (
+        <>
+            <h1>{article.title}</h1>
+            {article.content.map((paragraph, key) => (
+                <p key={key}>{paragraph}</p>
+            ))}
+        </>
+    )
+}
 
 export default ArticlePage;
