02c6376493122f6784fe999e9f30b4ad918b9565
Adam Vorkel <adamvorkel@gmail.com>
2020-09-15 23:11:22 +0200

    Article model dynamically generates unique slug on save

--- api/src/models/Article.js
+++ api/src/models/Article.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+mongoose.set('useCreateIndex', true);
+
 const ArticleSchema = new mongoose.Schema({
     title: {
         type: String,
@@ -15,8 +17,30 @@ const ArticleSchema = new mongoose.Schema({
     posted: {
         type: Date,
         default: Date.now()
+    },
+    slug: {
+        type: String,
+        index: true,
+        unique: true,
+        required: true,
+        trim: true
     }
 });
+
+ArticleSchema.pre('validate', async function(next) {
+    this.slug = this.title
+    .toString()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w\-]+/g, '')
+    .replace(/\-\-+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '');
+    
+    const other = await this.constructor.countDocuments({ slug: { $regex: `^${this.slug}` } });
+    if(other) this.slug = `${this.slug}-${other}` 
+    next();
+})
 
 const Article = mongoose.model('Article', ArticleSchema);
 
--- api/src/server.js
+++ api/src/server.js
@@ -11,9 +11,14 @@ const PORT = process.env.PORT || 8000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.get('/api/articles/:title', async (req, res) => {
-//     const title = req.params.title;
-// });
+app.get('/api/articles/:slug', async (req, res) => {
+    const slug = req.params.slug;
+    try {
+        const article = await Article.findOne({ slug })
+    } catch(error) {
+        console.log('Failed to get article')
+    }
+});
 
 // app.post('/api/articles', async (req, res) => {
   
